ff45e13d059f960f5ce505587643377edc4f5c44
Joshua Shapiro <joshua.shapiro@gmail.com>
2024-09-30 13:33:23 -0700

    Added aws secret fetch to postgres config

--- query_builder/postgres_config.py
+++ query_builder/postgres_config.py
@@ -2,13 +2,61 @@
 Postgres Configuration
 """
 from contextlib import contextmanager
+import json
 import psycopg2
 from psycopg2.extras import DictCursor
+import boto3
+from botocore.exceptions import ClientError
+
+
+def get_secret(secret_name, region_name) -> dict:
+    """
+    Returns a dictionary with {username, password} for 
+    """
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    return json.loads(secret)
+
 
 class PostgresConfig:
-    """Configurations required for the Postgres client."""
+    """
+    Configurations required for the Postgres client.
+    There are multiple ways to specify the configuration.
+
+    1. Using a DSN string
+        - dsn 
+    2. Using individual parameters for each configuration value
+        - dbname
+        - user
+        - password
+        - host
+        - port (optional)
+    3. Using AWS Secrets Manager to store the username and password
+        - aws_secret_name
+        - aws_region_name
+        - dname
+        - host
+        - port (optional)
+    """
 
     def __init__(self,
+                 aws_secret_name: str = None,
+                 aws_region_name: str = None,
                  dbname: str = None,
                  user: str = None,
                  password: str = None,
@@ -18,6 +66,8 @@ class PostgresConfig:
                  ):
         """Configuration values for the Postgres client."""
         self.dbname = dbname
+        self.aws_secret_name = aws_secret_name
+        self.aws_region_name = aws_region_name
         self.user = user
         self.password = password
         self.host = host
@@ -31,6 +81,12 @@ class PostgresConfig:
             return {'dsn': self.dsn}
 
         params = {}
+
+        if self.aws_secret_name is not None and self.aws_region_name is not None:
+            secret = get_secret(self.aws_secret_name, self.aws_region_name)
+            params['user'] = secret['username']
+            params['password'] = secret['password']
+            
         if self.dbname is not None:
             params['dbname'] = self.dbname
         if self.user is not None:
